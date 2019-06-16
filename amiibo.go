6708927596e226126b123e395b3617a66d7af0ee
gellel <lindsay.gelle@gmail.com>
2019-06-17 00:27:41 +1000

    Initial commit

---
+++ amiibo.go
@@ -0,0 +1,12 @@
+package main
+
+type Amiibo struct {
+	Character string
+	Game      string
+	Head      string
+	Image     string
+	Name      string
+	Release   Release
+	Series    string
+	Type      string
+}
---
+++ release.go
@@ -0,0 +1,10 @@
+package main
+
+import "time"
+
+type Release struct {
+	AU time.Time
+	EU time.Time
+	JP time.Time
+	NA time.Time
+}
