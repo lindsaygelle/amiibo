627c8728c4e3dd2c1f025d9e2cae9d5a2dbb8d42
gellel <lindsay.gelle@gmail.com>
2019-06-19 00:57:06 +1000

    Adding required Amiibo fields

--- amiibo.go
+++ amiibo.go
@@ -1,18 +1,36 @@
 package main
 
-func NewAmiibo() Amiibo {
+import "time"
 
-	return Amiibo{}
+func newAmiibo() *Amiibo {
+	return &Amiibo{Release: newRelease()}
+}
+
+func NewAmiibo(amiibo, character, game, head, image, name, series, tail, t, URL string, AU, EU, JP, NA time.Time) *Amiibo {
+	return &Amiibo{
+		Amiibo:    amiibo,
+		Character: character,
+		Game:      game,
+		Head:      head,
+		Image:     image,
+		Name:      name,
+		Release:   NewRelease(AU, EU, JP, NA),
+		Series:    series,
+		Tail:      tail,
+		Type:      t,
+		URL:       URL}
 }
 
 type Amiibo struct {
-	API       string  `json:"api"`
-	Character string  `json:"character"`
-	Game      string  `json:"game"`
-	Head      string  `json:"head"`
-	Image     string  `json:"image"`
-	Name      string  `json:"name"`
-	Release   Release `json:"release"`
-	Series    string  `json:"series"`
-	Type      string  `json:"type"`
+	Amiibo    string   `json:"amiiboSeries"`
+	Character string   `json:"character"`
+	Game      string   `json:"gameSeries"`
+	Head      string   `json:"head"`
+	Image     string   `json:"image"`
+	Name      string   `json:"name"`
+	Release   *Release `json:"release"`
+	Series    string   `json:"series"`
+	Tail      string   `json:"tail"`
+	Type      string   `json:"type"`
+	URL       string   `json:"URL"`
 }
