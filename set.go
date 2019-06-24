db54e1d31a6bb73cc02403e16cf8f902c5700d71
gellel <lindsay.gelle@gmail.com>
2019-06-24 13:18:21 +1000

    Updating Set interface

--- set.go
+++ set.go
@@ -1,12 +1,20 @@
 package amiibo
 
-import "github.com/gellel/lexicon"
+import (
+	"github.com/gellel/lexicon"
+	"github.com/gellel/slice"
+)
 
 type set interface {
 	Add(amiibo *Amiibo) *Set
 	Del(key string) bool
+	Each(f func(key string, slice *Slice)) *Set
 	Get(key string) *Slice
 	Has(key string) bool
+	Keys() *slice.String
+	Len() int
+	Map(f func(key string, slice *Slice) *Slice) *Set
+	Values() *Slice
 }
 
 type Set struct {
