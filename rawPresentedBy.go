2d8fd755388dba0d2c6878bb2e3028e8cd92e6f9
gellel <lindsay.gelle@gmail.com>
2019-06-29 22:01:26 +1000

    Updated RawPresentedBy String method

--- rawPresentedBy.go
+++ rawPresentedBy.go
@@ -1,6 +1,9 @@
 package amiibo
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 var (
 	_ rawAmiiboPresentedBy = (*RawAmiiboPresentedBy)(nil)
@@ -12,5 +15,5 @@ type rawAmiiboPresentedBy interface{}
 type RawAmiiboPresentedBy string
 
 func (r *RawAmiiboPresentedBy) String() string {
-	return fmt.Sprintf("%s", *r)
+	return fmt.Sprintf("%s", strings.Replace(string(*r), "noa:publisher/", "", -1))
 }
