6036df683a141066561c219ca99d4b7e22ca68a8
gellel <lindsay.gelle@gmail.com>
2019-07-01 19:51:06 +1000

    Updating data structure

--- amiiboMap.go
+++ amiiboMap.go
@@ -1,6 +1,19 @@
 package amiibo
 
-import "github.com/gellel/lexicon"
+import (
+	"github.com/gellel/lexicon"
+	"github.com/gellel/slice"
+)
+
+var (
+	_ amiiboMap = (*AmiiboMap)(nil)
+)
+
+func newAmiiboMap() *AmiiboMap {
+	return &AmiiboMap{lexicon: &lexicon.Lexicon{}}
+}
+
+type amiiboMap interface{}
 
 type AmiiboMap struct {
 	lexicon *lexicon.Lexicon
@@ -37,4 +50,31 @@ func (pointer *AmiiboMap) Get(HEX string) (*Amiibo, bool) {
 
 func (pointer *AmiiboMap) Has(HEX string) bool {
 	return pointer.lexicon.Has(HEX)
+}
+
+func (pointer *AmiiboMap) Intersection(amiiboMap *AmiiboMap) *AmiiboMap {
+	return &AmiiboMap{lexicon: pointer.lexicon.Intersection(amiiboMap.lexicon)}
+}
+
+func (pointer *AmiiboMap) Keys() *slice.String {
+	return pointer.lexicon.Keys()
+}
+
+func (pointer *AmiiboMap) Len() int {
+	return pointer.lexicon.Len()
+}
+
+func (pointer *AmiiboMap) Map(f func(HEX string, amiibo *Amiibo) *Amiibo) *AmiiboMap {
+	pointer.lexicon.Map(func(key string, value interface{}) interface{} {
+		return f(key, value.(*Amiibo))
+	})
+	return pointer
+}
+
+func (pointer *AmiiboMap) Peek(HEX string) string {
+	return pointer.lexicon.Peek(HEX)
+}
+
+func (pointer *AmiiboMap) Values() *AmiiboSlice {
+	return &AmiiboSlice{slice: pointer.lexicon.Values()}
 }
--- amiiboSlice.go
+++ amiiboSlice.go
@@ -2,6 +2,21 @@ package amiibo
 
 import "github.com/gellel/slice"
 
+var (
+	_ amiiboSlice = (*AmiiboSlice)(nil)
+)
+
+func newAmiiboSlice() *AmiiboSlice {
+	return &AmiiboSlice{slice: &slice.Slice{}}
+}
+
+type amiiboSlice interface{}
+
 type AmiiboSlice struct {
 	slice *slice.Slice
+}
+
+func (pointer *AmiiboSlice) Append(amiibo *Amiibo) *AmiiboSlice {
+	pointer.slice.Append(amiibo)
+	return pointer
 }
