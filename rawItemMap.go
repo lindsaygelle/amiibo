65f871d4fb425e91beea2e847b9ec077a89400eb
gellel <lindsay.gelle@gmail.com>
2019-07-04 11:48:03 +1000

    Updating RawItemMap struct

--- rawItemMap.go
+++ rawItemMap.go
@@ -1,6 +1,9 @@
 package amiibo
 
-import "github.com/gellel/lexicon"
+import (
+	"github.com/gellel/lexicon"
+	"github.com/gellel/slice"
+)
 
 func getRawItemMap(content *[]byte) *RawItemMap {
 	rawPayload, err := unmarshallRawPayload(content)
@@ -18,6 +21,22 @@ func newRawItemMap() *RawItemMap {
 	return &RawItemMap{lexicon: &lexicon.Lexicon{}}
 }
 
+type rawItemMap interface {
+	Add(rawItem *RawItem) *RawItemMap
+	Del(rawItem *RawItem) bool
+	Each(f func(key string, rawItem *RawItem)) *RawItemMap
+	Fetch(key string) *RawItem
+	Get(key string) (*RawItem, bool)
+	Has(key string) bool
+	Intersection(rawItemMap *RawItemMap) *RawItemMap
+	Keys() *slice.String
+	Len() int
+	Map(f func(key string, rawItem *RawItem) *RawItem) *RawItemMap
+	Peek(key string) string
+	String() string
+	Values() *RawItemSlice
+}
+
 type RawItemMap struct {
 	lexicon *lexicon.Lexicon
 }
@@ -25,4 +44,40 @@ type RawItemMap struct {
 func (pointer *RawItemMap) Add(rawItem *RawItem) *RawItemMap {
 	pointer.lexicon.Add(rawItem.Title, rawItem)
 	return pointer
+}
+
+func (pointer *RawItemMap) Del(rawItem *RawItem) bool {
+	return pointer.lexicon.Del(rawItem.Title)
+}
+
+func (pointer *RawItemMap) Each(f func(key string, rawItem *RawItem)) *RawItemMap {
+	pointer.lexicon.Each(func(key string, value interface{}) {
+		f(key, value.(*RawItem))
+	})
+	return pointer
+}
+
+func (pointer *RawItemMap) Fetch(key string) *RawItem {
+	rawItem, _ := pointer.Get(key)
+	return rawItem
+}
+
+func (pointer *RawItemMap) Get(key string) (*RawItem, bool) {
+	value, ok := pointer.lexicon.Get(key)
+	if ok {
+		return value.(*RawItem), ok
+	}
+	return nil, ok
+}
+
+func (pointer *RawItemMap) Has(key string) bool {
+	return pointer.lexicon.Has(key)
+}
+
+func (pointer *RawItemMap) Intersection(rawItemMap *RawItemMap) *RawItemMap {
+	return &RawItemMap{lexicon: pointer.lexicon.Intersection(rawItemMap.lexicon)}
+}
+
+func (pointer *RawItemMap) Keys() *slice.String {
+	return pointer.lexicon.Keys()
 }
