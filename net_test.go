1faebfdc82d27d47c976e450a6654622cd6780e7
gellel <lindsay.gelle@gmail.com>
2019-07-07 22:19:23 +1000

    Adding test parameters

--- net_test.go
+++ net_test.go
@@ -3,16 +3,32 @@ package amiibo
 import (
 	"fmt"
 	"net/http"
+	"strings"
 	"testing"
 )
 
 func TestNet(t *testing.T) {
 
-	resp, err := http.Get(amiiboURL)
+	client := new(http.Client)
+
+	req, err := http.NewRequest(http.MethodOptions, amiiboURL, nil)
+
 	if ok := err == nil; !ok {
 		t.Fatalf(fmt.Sprintf("%v", err))
 	}
-	if ok := resp.StatusCode == 200; !ok {
-		t.Fatalf(fmt.Sprintf("Amiibo URI is not responding %s", resp.Status))
+
+	resp, err := client.Do(req)
+
+	if ok := err == nil; !ok {
+		t.Fatalf(fmt.Sprintf("%v", err))
+	}
+	if ok := resp.StatusCode == http.StatusOK; !ok {
+		t.Fatalf(fmt.Sprintf("%s is not responding %s", amiiboURL, resp.Status))
+	}
+
+	methodsPermitted := strings.Replace(resp.Header.Get("Allow"), ",", " ", -1)
+
+	if ok := strings.Contains(methodsPermitted, http.MethodGet); ok {
+		t.Fatalf(fmt.Sprintf("%s does not permit required HTTP method; only supports \"%v\"", amiiboURL, methodsPermitted))
 	}
 }
