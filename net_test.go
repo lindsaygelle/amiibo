81a320aef9518d8904ebfc69c39eba326673af86
gellel <lindsay.gelle@gmail.com>
2019-07-07 22:03:05 +1000

    Adding simple testcase

--- net_test.go
+++ net_test.go
@@ -1,7 +1,18 @@
 package amiibo
 
-import "testing"
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
 
 func TestNet(t *testing.T) {
 
+	resp, err := http.Get(amiiboURL)
+	if ok := err == nil; !ok {
+		t.Fatalf(fmt.Sprintf("%v", err))
+	}
+	if ok := resp.StatusCode == 200; !ok {
+		t.Fatalf(fmt.Sprintf("Amiibo URI is not responding %s", resp.Status))
+	}
 }
