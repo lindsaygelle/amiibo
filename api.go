e81c0026dcdbb130409a6afb0ff5dab478ad3d17
Lindsay Gelle <lindsay.gelle@gmail.com>
2019-07-27 20:29:34 +1000

    Updating API

--- api.go
+++ api.go
@@ -2,8 +2,12 @@ package amiibo
 
 func NewAPI() {}
 
+// API defines the methods for an API struct.
 type api interface{}
 
+// API stores the raw Amiibo & Item information that is contained in the Nintendo XHR HTTP response.
+// API provides a single point of reference for data transactions that can be performed on the
+// internal raw Amiib & raw Item pointers. 
 type API struct {
 	rawPayload *RawPayload
 }
