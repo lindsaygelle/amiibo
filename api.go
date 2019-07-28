a5fb1cd2e63d99a3a91e60181a6726283f0a1566
gellel <lindsay.gelle@gmail.com>
2019-07-28 17:45:19 +1000

    Updating API package

--- api.go
+++ api.go
@@ -1,13 +1,22 @@
 package amiibo
 
-func NewAPI() {}
+var (
+	_ api = (*API)(nil)
+)
+
+// NewAPI returns a new Amiibo API pointer.
+func NewAPI() *API {
+	return &API{}
+}
 
 // API defines the methods for an API struct.
-type api interface{}
+type api interface {
+	Get() error
+}
 
 // API stores the raw Amiibo & Item information that is contained in the Nintendo XHR HTTP response.
 // API provides a single point of reference for data transactions that can be performed on the
-// internal raw Amiib & raw Item pointers. 
+// internal raw Amiib & raw Item pointers.
 type API struct {
 	rawPayload *RawPayload
 }
@@ -18,7 +27,11 @@ func (pointer *API) Get() error {
 		if err != nil {
 			return err
 		}
-		NewRawPayload(*b...)
+		r, err := unmarshallRawPayload(b)
+		if err != nil {
+			return err
+		}
+		pointer.rawPayload = r
 	}
 	return nil
 }
--- api_test.go
+++ api_test.go
@@ -1,7 +1,18 @@
 package amiibo_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/gellel/amiibo"
+)
 
 func TestAPI(t *testing.T) {
 
+	api := amiibo.NewAPI()
+
+	err := api.Get()
+
+	if err != nil {
+		t.Fatalf("t.Fatal: amiibo.API.Get() returned err")
+	}
 }
