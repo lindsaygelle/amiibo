e6c5ccb1a4798cf92b50bb88412251debb4c103e
gellel <lindsay.gelle@gmail.com>
2019-08-06 22:51:34 +1000

    Added type check test

--- rawAmiibo_test.go
+++ rawAmiibo_test.go
@@ -12,5 +12,15 @@ func TestRawAmiibo(t *testing.T) {
 	b := []byte(rawAmiiboDefault)
 	rawAmiibo := amiibo.NewRawAmiibo(&b)
 
-	fmt.Println(reflect.TypeOf(rawAmiibo))
+	if ok := reflect.ValueOf(rawAmiibo).Kind() == reflect.Ptr; ok != true {
+		t.Fatalf("amiibo.NewRawAmiibo(b *[]byte) *amiibo.RawAmiibo != uintptr")
+	}
+
+	returnType := reflect.TypeOf(rawAmiibo).Elem().String()
+
+	expectType := reflect.TypeOf(&amiibo.RawAmiibo{}).Elem().String()
+
+	if ok := returnType == expectType; ok != true {
+		t.Fatalf(fmt.Sprintf("amiibo.NewRawAmiibo(b *[]byte) %s != %s", returnType, expectType))
+	}
 }
