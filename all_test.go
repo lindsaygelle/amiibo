4aac34c03b937dce2ad458b955d4a7abd95b89ce
gellel <lindsay.gelle@gmail.com>
2019-08-13 21:46:13 +1000

    Adding test data

--- all_test.go
+++ all_test.go
@@ -1,6 +1,28 @@
 package amiibo_test
 
 const (
+	defaultAmiiboName          string = "Toon Link - The Wind Waker"
+	defaultAmiiboPage          string = "/amiibo/detail/toon-link…30th-anniversary-series"
+	defaultBoxArtURL           string = "/content/dam/noa/en_US/a…nk-WW_30thAnniv_box.png"
+	defaultDetailsPath         string = "/content/noa/en_US/amiib…30th-anniversary-series"
+	defaultDetailsURL          string = "/amiibo/detail/toon-link…30th-anniversary-series"
+	defaultFigureURL           string = "/content/dam/noa/en_US/a…k-WW_30thAnniv_char.png"
+	defaultFranchise           string = "The Legend of Zelda"
+	defaultGameCode            string = "NVLEAK2A"
+	defaultHexCode             string = "#ffdc81"
+	defaultIsReleased          string = "true"
+	defaultOverviewDescription string = "<p style=\"margin-top: 0;\">This amiibo figure shows Link™ in his toon-shaded incarnation from The Legend of Zelda™: The Wind Waker™ game. Link holds the Wind Waker itself, a wand that allowed him to control the winds.</p>"
+	defaultPresentedBy         string = "noa:publisher/nintendo"
+	defaultPrice               string = "24.99"
+	defaultReleaseDateMask     string = "12/02/2016"
+	defaultSeries              string = "30th anniversary"
+	defaultSlug                string = "toon-link-the-wind-waker…30th-anniversary-series"
+	defaultType                string = "Figure"
+	defaultUnixTimestamp       string = "1480636800"
+	defaultUpc                 string = "045496893064"
+)
+
+const (
 	rawAmiiboDefault string = `{
 		"amiiboName": "Toon Link - The Wind Waker",
 		"amiiboPage": "/amiibo/detail/toon-link…30th-anniversary-series",
--- amiibo_test.go
+++ amiibo_test.go
@@ -6,6 +6,10 @@ import (
 
 func TestAmiibo(t *testing.T) {
 
+	rawAmiibo := &RawAmiibo{}
+
+	newAmiibo(rawAmiibo)
+
 	/*writeAmiibo(&Amiibo{Hex: "#ffdc81"})
 	fmt.Println(getAmiibo("#ffdc81"))
 	fmt.Println(deleteAmiibo(&Amiibo{Hex: "#ffdc81"}))*/
