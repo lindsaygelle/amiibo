ed9913380007a4fbb5165f897af4e978c6ab0152
gellel <lindsay.gelle@gmail.com>
2019-08-19 21:13:20 +1000

    Adding test cases

--- amiiboMap_test.go
+++ amiiboMap_test.go
@@ -12,7 +12,13 @@ func TestAmiiboMap(t *testing.T) {
 		t.Fatalf("amiibo.newAmiiboMap() test cannot run testAmiiboStruct is nil")
 	}
 
-	amiiboMap.Add(testAmiiboStruct)
+	if m := amiiboMap.Add(testAmiiboStruct); m != amiiboMap {
+		t.Fatalf("AmiiboMap.Add(a *Amiibo) *AmiiboMap != %v", amiiboMap)
+	}
+
+	if n := amiiboMap.Len(); n < 1 {
+		t.Fatalf("AmiiboMap.Len() int < 1")
+	}
 
 	if _, ok := (*amiiboMap.lexicon)[testAmiiboStruct.ID]; ok != true {
 		t.Fatalf("AmiiboMap.Add(a *Amiibo) *AmiiboMap does not have key where ID = %s", testAmiiboStruct.ID)
