95a45e7cea30639f7dfa7bdf02a2aac03bcb1ca2
gellel <lindsay.gelle@gmail.com>
2019-08-20 20:48:58 +1000

    Added test hash

--- amiibo_test.go
+++ amiibo_test.go
@@ -1,6 +1,7 @@
 package amiibo
 
 import (
+	"crypto/md5"
 	"fmt"
 	"reflect"
 	"testing"
@@ -13,11 +14,16 @@ func TestAmiibo(t *testing.T) {
 	if testAmiiboStruct == nil {
 		t.Fatalf("amiibo.NewAmiibo(r *amiibo.RawAmiibo) *amiibo.Amiibo == nil")
 	}
+
 	returnType := reflect.TypeOf(testAmiiboStruct).Elem().String()
 
 	expectType := reflect.TypeOf(&Amiibo{}).Elem().String()
 
 	if ok := returnType == expectType; ok != true {
 		t.Fatalf(fmt.Sprintf("amiibo.NewAmiibo(r *amiibo.RawAmiibo) %s != %s", returnType, expectType))
+	}
+
+	if hashMD5 := fmt.Sprintf("%x", md5.Sum([]byte(rawAmiiboStructDefault.AmiiboName))); hashMD5 != testAmiiboStruct.ID {
+		t.Fatalf(fmt.Sprintf("Amiibo.ID != md5.Sum([]byte(RawAmiibo.AmiiboName); %s != %s", testAmiiboStruct.ID, hashMD5))
 	}
 }
