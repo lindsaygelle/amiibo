a8ae260419bd9d6fa02fcf122473e6e3e2c9869a
gellel <lindsay.gelle@gmail.com>
2019-08-25 18:04:13 +1000

    Adding var documentation

--- var.go
+++ var.go
@@ -12,7 +12,10 @@ var (
 )
 
 var (
-	reStripHTML   = regexp.MustCompile(`(<[^>]*>|\n(\s{1,})?)`)
-	reStripName   = regexp.MustCompile(`(\&\#[0-9]+\;|™)`)
+	// reStripHTML is the regex pattern that matches all valid HTML patterns.
+	reStripHTML = regexp.MustCompile(`(<[^>]*>|\n(\s{1,})?)`)
+	// reStripName is the regex pattern that matches all unsupported characters in an Amiibo or Item's name.
+	reStripName = regexp.MustCompile(`(\&\#[0-9]+\;|™)`)
+	// reStripSpaces is the regexp pattern that matches all double or n following whitespace.
 	reStripSpaces = regexp.MustCompile(`\s{2,}`)
 )
