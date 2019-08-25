459dacde6ce97356f1d5853a08770ee556b3f304
gellel <lindsay.gelle@gmail.com>
2019-08-25 18:02:09 +1000

    Adding AmiiboMap tests

--- amiiboMap_test.go
+++ amiiboMap_test.go
@@ -29,7 +29,15 @@ func TestAmiiboMap(t *testing.T) {
 		t.Fatalf("AmiiboMap.Intersection(m *AmiiboMap) *AmiiboMap did not share a known intersection between AmiiboMap A and AmiiboMap B")
 	}
 
+	if ok := testAmiiboStruct.Name == amiiboMap.Peek(testAmiiboStruct.ID); ok != true {
+		t.Fatalf("AmiiboMap.Fetch(ID string) string does not return the expected hash; %v != %v", amiiboMap.Fetch(testAmiiboStruct.ID), testAmiiboStruct.String())
+	}
+
 	if ok := (amiiboMap.Del(testAmiiboStruct) && (amiiboMap.Has(testAmiiboStruct.ID) == false)); ok != true {
 		t.Fatalf("AmiiboMap.Del(a *Amiibo) bool did not delete the assigned Amiibo pointer")
+	}
+
+	if ok := amiiboMap.Empty(); ok != true {
+		t.Fatalf("AmiiboMap.Empty() bool != true; there should be no items in the AmiiboMap")
 	}
 }
