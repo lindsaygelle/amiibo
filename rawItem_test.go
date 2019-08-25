a8e93c1e15d39f8a055a95bcc720df659c39cebf
gellel <lindsay.gelle@gmail.com>
2019-08-25 17:50:35 +1000

    Adding RawItem tests

--- rawItem_test.go
+++ rawItem_test.go
@@ -1,5 +1,26 @@
 package amiibo
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
 
-func TestRawItem(t *testing.T) {}
+func TestRawItem(t *testing.T) {
+
+	b := []byte(rawItemDefault)
+
+	rawItem := NewRawItem(&b)
+
+	if ok := reflect.ValueOf(rawItem).Kind() == reflect.Ptr; ok != true {
+		t.Fatalf("amiibo.NewRawItem(b *[]byte) *amiibo.RawItem != uintptr")
+	}
+
+	returnType := reflect.TypeOf(rawItem).Elem().String()
+
+	expectType := reflect.TypeOf(&RawItem{}).Elem().String()
+
+	if ok := returnType == expectType; ok != true {
+		t.Fatalf(fmt.Sprintf("amiibo.NewRawItem(b *[]byte) %s != %s", returnType, expectType))
+	}
+}
