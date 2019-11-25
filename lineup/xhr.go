f8053b6f02ca2d0842954cad261ba57e714d25c5
gellel <lindsay.gelle@gmail.com>
2019-11-26 09:43:46 +1100

    Adding verbosity to XHR documentation

--- lineup/xhr.go
+++ lineup/xhr.go
@@ -5,7 +5,7 @@ import "net/http"
 // XHR is the content HTTP response that is requested from https://www.nintendo.com/amiibo/line-up/.
 // XHR contains the structured information provided from Nintendo as-is and is updated
 // with the release of new Nintendo Amiibo products. All content within the XHR
-// response body describes the Amiibo content in the context of the lineup, meaning information
+// response body describes the Nintendo Amiibo products in the context of the lineup, meaning information
 // such as the compatibility of the figuring is not offered.
 type XHR struct {
 	Amiibo           []*Amiibo      `json:"amiiboList"`
