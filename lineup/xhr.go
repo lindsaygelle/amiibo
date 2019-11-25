9891d3b1469b6e66ae38fda986e1a9b196e15eec
gellel <lindsay.gelle@gmail.com>
2019-11-25 21:24:22 +1100

    Added additional fields returned from the HTTP response

--- lineup/xhr.go
+++ lineup/xhr.go
@@ -1,15 +1,19 @@
 package lineup
 
+import "net/http"
+
 // XHR is the content HTTP response that is requested from https://www.nintendo.com/amiibo/line-up/.
 // XHR contains the structured information provided from Nintendo as-is and is updated
 // with the release of new Nintendo Amiibo products. All content within the XHR
 // response body describes the Amiibo content in the context of the lineup, meaning information
 // such as the compatibility of the figuring is not offered.
 type XHR struct {
-	Amiibo           []*Amiibo `json:"amiiboList"`
-	ComponentPath    string    `json:"componentPath"`
-	DateFormatString string    `json:"dataFormatString"`
-	Items            []*Item   `json:"items"`
-	Status           string    `json:"status"`
-	StatusCode       string    `json:"statusCode"`
+	Amiibo           []*Amiibo      `json:"amiiboList"`
+	ComponentPath    string         `json:"componentPath"`
+	ContentLength    int64          `json:"contentLength"`
+	Cookies          []*http.Cookie `json:"cookies"`
+	DateFormatString string         `json:"dataFormatString"`
+	Items            []*Item        `json:"items"`
+	Status           string         `json:"status"`
+	StatusCode       string         `json:"statusCode"`
 }
