45e4e53767e609733ce3e493d93b290f2b6af511
gellel <lindsay.gelle@gmail.com>
2019-11-25 21:17:36 +1100

    Added lineup HTTP get fn

---
+++ lineup/lineup.go
@@ -0,0 +1,37 @@
+package lineup
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gellel/amiibo/network"
+)
+
+// Get performs a HTTP request to Nintendo Amiibo lineup resource and unmarshal's the
+// HTTP response body on http.StatusOK. Throws an error if the Nintendo server
+// returns anything other than http.StatusOK. If the response content cannot be
+// handled by json.Unmarshal the corresponding error message is returned.
+func Get() (*XHR, error) {
+	var (
+		data     []byte
+		res, err = network.Client.Do(Request)
+		xhr      XHR
+	)
+	if err != nil {
+		return nil, err
+	}
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf(res.Status)
+	}
+	data, err = ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(data, &xhr)
+	if err != nil {
+		return nil, err
+	}
+	return &xhr, err
+}
