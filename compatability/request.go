52c1187161a4d100c2c2ae4f221751d2678440c1
gellel <lindsay.gelle@gmail.com>
2019-11-25 21:19:20 +1100

    Added defined http.Request address to Nintendo compatability content

---
+++ compatability/request.go
@@ -0,0 +1,12 @@
+package compatability
+
+import (
+	"net/http"
+
+	"github.com/gellel/amiibo/resource"
+)
+
+var (
+	// Request is the HTTP request made to collect the XHR content from https://www.nintendo.com/.
+	Request, _ = http.NewRequest(http.MethodGet, resource.Compatability, nil)
+)
