a8cda16b5ad2adf7edcf9b94e9bf9cc5cb9039b1
gellel <lindsay.gelle@gmail.com>
2019-11-25 21:20:12 +1100

    Added common resources

---
+++ resource/resource.go
@@ -0,0 +1,15 @@
+package resource
+
+const (
+	// Compatability is the URL to the XHR content required for compatability.XHR.
+	Compatability string = Nintendo + "/" + "content/noa/en_US/amiibo/compatibility/jcr:content/root/responsivegrid/compatibility_chart.model.json"
+)
+
+const (
+	// Lineup is the URL to the XHR content required for lineup.XHR.
+	Lineup string = Nintendo + "/" + "content/noa/en_US/amiibo/line-up/jcr:content/root/responsivegrid/lineup.model.json"
+)
+const (
+	// Nintendo is the URL to the official Nintendo website.
+	Nintendo string = "https://www.nintendo.com"
+)
