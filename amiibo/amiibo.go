eaa8b1955e914754e908a0ec23bc270930f8ff62
gellel <lindsay.gelle@gmail.com>
2019-11-26 17:27:27 +1100

    Breaking apart monolith

--- amiibo/amiibo.go
+++ amiibo/amiibo.go
@@ -3,17 +3,42 @@ package amiibo
 import (
 	"fmt"
 	"reflect"
+	"strconv"
+	"strings"
 	"time"
 
+	"golang.org/x/text/currency"
 	"golang.org/x/text/language"
 
 	"github.com/gellel/amiibo/address"
+	"github.com/gellel/amiibo/compatability"
 	"github.com/gellel/amiibo/image"
+	"github.com/gellel/amiibo/lineup"
+	"github.com/gellel/amiibo/resource"
+	t "github.com/gellel/amiibo/text"
+)
+
+const (
+	noa string = "/content/noa/en_US/" // Nintendo of America prefix.
+)
+
+const (
+	rep string = ""     // rep string
+	sep string = "."    // sep string
+	tep string = "%s%s" // tep string for raw url
 )
 
 const (
 	// Version is the semver of amiibo.Amiibo.
 	Version string = "1.0.0"
+)
+
+var (
+	currencyISO = currency.USD.String() // Currency ISO for all Nintendo Amiibo products.
+)
+
+var (
+	errNilAll = fmt.Errorf("*c, *l and *i are nil")
 )
 
 // Amiibo is a structured representation of a Nintendo Amiibo figuring.
@@ -64,4 +89,190 @@ func (a *Amiibo) Get(key string) string {
 	var r = reflect.ValueOf(a)
 	var v = reflect.Indirect(r).FieldByName(key)
 	return fmt.Sprintf("%s", v)
+}
+
+func NewAmiibo(c *compatability.Amiibo, l *lineup.Amiibo, i *lineup.Item) (*Amiibo, error) {
+	var (
+		ok bool
+	)
+	ok = (c != nil) || (l != nil) || (i != nil)
+	if !ok {
+		return nil, errNilAll
+	}
+	var (
+		a               *Amiibo
+		boxImage        *image.Image
+		compatability   []*Game
+		complete        bool
+		description     string
+		detailsPath     string
+		detailsURL      *address.Address
+		figureURL       *address.Address
+		franchise       string
+		franchiseID     string
+		gameCode        string
+		hex             string
+		ID              string
+		img             *image.Image
+		isRelatedTo     string
+		isReleased      bool
+		language        = language.AmericanEnglish
+		lastModified    int64
+		name            string
+		overview        string
+		pageURL         *address.Address
+		path            string
+		presentedBy     string
+		price           string
+		rawurl          string
+		releaseDateMask string
+		series          string
+		seriesID        string
+		slug            string
+		tagID           string
+		timestamp       time.Time
+		typeAlias       string
+		typeOf          string
+		unix            int64
+		UPC             string
+		URI             string
+		URL             *address.Address
+	)
+	complete = (c != nil) && (l != nil) && (i != nil)
+	if c != nil {
+		ID = c.ID
+		img, _ = image.NewImage(fmt.Sprintf(tep, resource.Nintendo, c.Image))
+		isRelatedTo = c.IsRelatedTo
+		isReleased, _ = strconv.ParseBool(c.IsReleased)
+		name = t.Name(c.Name)
+		tagID = c.TagID
+		typeOf = c.Type
+		URL, _ = address.NewAddress(fmt.Sprintf(tep, resource.Nintendo, c.URL))
+	}
+	if l != nil {
+		boxImage, _ = parseAmiiboBoxImage(l)
+		detailsPath = parseAmiiboDetailsPath(l)
+		detailsURL, _ = parseAmiiboDetailsURL(l)
+		figureURL, _ = parseAmiiboFigureURL(l)
+		franchise = parseAmiiboFranchise(l)
+		franchiseID = parseAmiiboFranchiseID(l)
+		gameCode = parseAmiiboGameCode(l)
+		hex = parseAmiiboHexCode(l)
+		isReleased = l.IsReleased
+		ok = (len(name) != 0)
+		if !ok {
+			name = parseAmiiboName(l)
+		}
+		overview = parseAmiiboOverviewDescription(l)
+		pageURL, _ = parseAmiiboPageURL(l)
+		presentedBy = t.PresentedBy(l.PresentedBy)
+		price = l.Price
+		releaseDateMask = l.ReleaseDateMask
+		series = l.Series
+		seriesID = t.URI(t.Name(series))
+		slug = l.Slug
+		timestamp = time.Unix(l.UnixTimestamp, 0).UTC()
+		typeAlias = strings.ToLower(l.Type)
+		UPC = l.UPC
+		unix = l.UnixTimestamp
+	}
+	if i != nil {
+		description = i.Description
+		lastModified = i.LastModified
+		path = i.Path
+		ok = (len(name) != 0)
+		if !ok {
+			name = t.Name(i.Title)
+		}
+		ok = (URL != nil)
+		if !ok {
+			rawurl = strings.TrimPrefix((i.URL + "/"), noa)
+			rawurl = fmt.Sprintf(tep, (resource.Amiibo + "/"), rawurl)
+			URL, _ = address.NewAddress(rawurl)
+		}
+	}
+	URI = t.URI(name)
+	compatability, _ = GetGames(URL.URL)
+	a = &Amiibo{
+		BoxImage:        boxImage,
+		Compatability:   compatability,
+		Complete:        complete,
+		Currency:        currencyISO,
+		Description:     description,
+		DetailsPath:     detailsPath,
+		DetailsURL:      detailsURL,
+		FigureURL:       figureURL,
+		Franchise:       franchise,
+		FranchiseID:     franchiseID,
+		GameCode:        gameCode,
+		HexCode:         hex,
+		ID:              ID,
+		Image:           img,
+		IsRelatedTo:     isRelatedTo,
+		IsReleased:      isReleased,
+		Language:        language,
+		LastModified:    lastModified,
+		Name:            name,
+		Overview:        overview,
+		Path:            path,
+		PageURL:         pageURL,
+		PresentedBy:     presentedBy,
+		Price:           price,
+		ReleaseDateMask: releaseDateMask,
+		Series:          series,
+		SeriesID:        seriesID,
+		Slug:            slug,
+		TagID:           tagID,
+		Timestamp:       timestamp,
+		Type:            typeOf,
+		TypeAlias:       typeAlias,
+		Unix:            unix,
+		UPC:             UPC,
+		URI:             URI,
+		URL:             URL}
+	return a, nil
+}
+
+func parseAmiiboBoxImage(l *lineup.Amiibo) (*image.Image, error) {
+	return image.NewImage(fmt.Sprintf(tep, resource.Nintendo, l.BoxArtURL))
+}
+
+func parseAmiiboDetailsPath(l *lineup.Amiibo) string {
+	return l.DetailsPath
+}
+
+func parseAmiiboDetailsURL(l *lineup.Amiibo) (*address.Address, error) {
+	return address.NewAddress(fmt.Sprintf(tep, resource.Nintendo, l.DetailsURL))
+}
+
+func parseAmiiboFigureURL(l *lineup.Amiibo) (*address.Address, error) {
+	return address.NewAddress(fmt.Sprintf(tep, resource.Nintendo, l.FigureURL))
+}
+
+func parseAmiiboFranchise(l *lineup.Amiibo) string {
+	return l.Franchise
+}
+
+func parseAmiiboFranchiseID(l *lineup.Amiibo) string {
+	return t.URI(t.Name(l.Franchise))
+}
+
+func parseAmiiboGameCode(l *lineup.Amiibo) string {
+	return l.GameCode
+}
+
+func parseAmiiboHexCode(l *lineup.Amiibo) string {
+	return l.HexCode
+}
+
+func parseAmiiboName(l *lineup.Amiibo) string {
+	return t.Name(l.AmiiboName)
+}
+
+func parseAmiiboOverviewDescription(l *lineup.Amiibo) string {
+	return t.Untokenize(l.OverviewDescription)
+}
+
+func parseAmiiboPageURL(l *lineup.Amiibo) (*address.Address, error) {
+	return address.NewAddress(fmt.Sprintf(tep, resource.Nintendo, l.AmiiboPage))
 }
