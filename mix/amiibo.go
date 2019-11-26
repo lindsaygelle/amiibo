3135764d4e5bbcefe4be4baa6a9465f7ad55a9f6
gellel <lindsay.gelle@gmail.com>
2019-11-26 18:31:29 +1100

    Adding documentation

--- mix/amiibo.go
+++ mix/amiibo.go
@@ -5,6 +5,8 @@ import (
 	"github.com/gellel/amiibo/lineup"
 )
 
+// Amiibo is an aggregation of all Amiibo related data points across the
+// various amiibo packages (amiibo/compatability, amiibo/lineup).
 type Amiibo struct {
 	Compatability *compatability.Amiibo
 	Item          *lineup.Item
--- mix/game.go
+++ mix/game.go
@@ -4,6 +4,8 @@ import (
 	"github.com/gellel/amiibo/compatability"
 )
 
+// Game is an aggregation of all game related data points across the
+// various amiibo packages (amiibo/compatability, amiibo/lineup).
 type Game struct {
 	Item *compatability.Item
 	Game *compatability.Game
