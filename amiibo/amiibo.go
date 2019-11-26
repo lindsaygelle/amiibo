c917b013b9e05bb4f1735fa31cb54b96314b44a4
gellel <lindsay.gelle@gmail.com>
2019-11-26 23:19:16 +1100

    Added content to amiibo/amiibo

--- amiibo/amiibo.go
+++ amiibo/amiibo.go
@@ -6,7 +6,10 @@ import (
 	"reflect"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
+
+	"github.com/gellel/amiibo/mix"
 
 	"golang.org/x/text/currency"
 	"golang.org/x/text/language"
@@ -49,43 +52,44 @@ var (
 // are provided from the amiibo/mix package.
 // Amiibos are consumed by amiibo/mux to create a basic HTTP REST API.
 type Amiibo struct {
-	BoxImage        *image.Image     `json:"box_image"`
-	Compatability   []*Game          `json:"compatability"`
-	Complete        bool             `json:"complete"`
-	Currency        string           `json:"currency"`
-	Description     string           `json:"description"`
-	DetailsPath     string           `json:"details_path"`
-	DetailsURL      *address.Address `json:"details_url"`
-	FigureURL       *address.Address `json:"figure_url"`
-	Franchise       string           `json:"franchise"`
-	FranchiseID     string           `json:"franchise_id"`
-	GameCode        string           `json:"game_code"`
-	HexCode         string           `json:"hex_code"`
-	ID              string           `json:"id"`
-	Image           *image.Image     `json:"image"`
-	IsRelatedTo     string           `json:"is_related_to"`
-	IsReleased      bool             `json:"is_released"`
-	Language        language.Tag     `json:"language"`
-	LastModified    int64            `json:"last_modified"`
-	Name            string           `json:"name"`
-	Overview        string           `json:"overview"`
-	PageURL         *address.Address `json:"page"`
-	Path            string           `json:"path"`
-	PresentedBy     string           `json:"presented_by"`
-	Price           float64          `json:"price"`
-	ReleaseDateMask string           `json:"release_date_mask"`
-	Series          string           `json:"series"`
-	SeriesID        string           `json:"series_id"`
-	Slug            string           `json:"slug"`
-	TagID           string           `json:"tag_id"`
-	Timestamp       time.Time        `json:"timestamp"`
-	Title           string           `json:"title"`
-	Type            string           `json:"type"`
-	TypeAlias       string           `json:"type_alias"`
-	Unix            int64            `json:"unix"`
-	UPC             string           `json:"upc"`
-	URI             string           `json:"uri"`
-	URL             *address.Address `json:"url"`
+	BoxImage              *image.Image     `json:"box_image"`
+	Compatability         []*Game          `json:"compatability"`
+	Complete              bool             `json:"complete"`
+	Currency              string           `json:"currency"`
+	Description           string           `json:"description"`
+	DetailsPath           string           `json:"details_path"`
+	DetailsURL            *address.Address `json:"details_url"`
+	FigureURL             *address.Address `json:"figure_url"`
+	Franchise             string           `json:"franchise"`
+	FranchiseID           string           `json:"franchise_id"`
+	GameCode              string           `json:"game_code"`
+	HexCode               string           `json:"hex_code"`
+	ID                    string           `json:"id"`
+	Image                 *image.Image     `json:"image"`
+	IsRelatedTo           string           `json:"is_related_to"`
+	IsReleased            bool             `json:"is_released"`
+	Language              language.Tag     `json:"language"`
+	LastModified          int64            `json:"last_modified"`
+	LastModifiedTimestamp time.Time        `json:"last_modified_timestamp"`
+	Name                  string           `json:"name"`
+	Overview              string           `json:"overview"`
+	PageURL               *address.Address `json:"page"`
+	Path                  string           `json:"path"`
+	PresentedBy           string           `json:"presented_by"`
+	Price                 float64          `json:"price"`
+	ReleaseDateMask       string           `json:"release_date_mask"`
+	Series                string           `json:"series"`
+	SeriesID              string           `json:"series_id"`
+	Slug                  string           `json:"slug"`
+	TagID                 string           `json:"tag_id"`
+	Timestamp             time.Time        `json:"timestamp"`
+	Title                 string           `json:"title"`
+	Type                  string           `json:"type"`
+	TypeAlias             string           `json:"type_alias"`
+	Unix                  int64            `json:"unix"`
+	UPC                   string           `json:"upc"`
+	URI                   string           `json:"uri"`
+	URL                   *address.Address `json:"url"`
 }
 
 // Get gets a field from the Amiibo by its struct name and returns its string value.
@@ -98,7 +102,7 @@ func (a *Amiibo) Get(key string) string {
 // NewAmiibo creates a new instance of the amiibo.Amiibo from the aggregation
 // of amiibo structs across the amiibo package. Returns an error if all data points are
 // not provided to the function.
-func NewAmiibo(c *compatability.Amiibo, l *lineup.Amiibo, i *lineup.Item) (*Amiibo, error) {
+func NewAmiibo(c *compatability.Amiibo, i *lineup.Item, l *lineup.Amiibo) (*Amiibo, error) {
 	var (
 		ok bool
 	)
@@ -121,8 +125,33 @@ func NewAmiibo(c *compatability.Amiibo, l *lineup.Amiibo, i *lineup.Item) (*Amii
 		parseItem(a, i)
 	}
 	a.Compatability, _ = parseAmiiboCompatability(a.URL.URL)
+	a.Complete = c != nil && i != nil && l != nil
 	a.URI = t.URI(a.Name)
 	return a, nil
+}
+
+// NewFromMix creates a sequence of amiibo.Amiibo in O(N) time. Omits all mix.Amiibo
+// that cannot be instantiated by amiibo.NewAmiibo.
+func NewFromMix(m map[string]*mix.Amiibo) []*Amiibo {
+	var (
+		s  = []*Amiibo{}
+		wg sync.WaitGroup
+	)
+	for _, m := range m {
+		wg.Add(1)
+		go func(m *mix.Amiibo) {
+			defer wg.Done()
+			var (
+				a, err = NewAmiibo(m.Compatability, m.Item, m.Lineup)
+			)
+			if err != nil {
+				return
+			}
+			s = append(s, a)
+		}(m)
+	}
+	wg.Wait()
+	return s
 }
 
 // parseAmiiboBoxImage parses the box art image from the lineup.Amiibo.
@@ -206,6 +235,11 @@ func parseAmiiboIsReleased(c *compatability.Amiibo) (bool, error) {
 	return strconv.ParseBool(c.IsReleased)
 }
 
+// parseAmiiboTimestamp parses the unix last modified timestamp from the lineup.Item.
+func parseAmiiboLastModifiedTimestamp(i *lineup.Item) time.Time {
+	return time.Unix(i.LastModified, 0).UTC()
+}
+
 // parseAmiiboName parses the name of the Amiibo using the name field from either compatability.Amiibo or lineup.Amiibo.
 func parseAmiiboName(s string) string {
 	return t.Name(s)
@@ -223,7 +257,7 @@ func parseAmiiboPageURL(l *lineup.Amiibo) (*address.Address, error) {
 
 // parseAmiiboPrice parses the Amiibo price from the lineup.Amiibo.
 func parseAmiiboPrice(l *lineup.Amiibo) (float64, error) {
-	return strconv.ParseFloat(strings.TrimPrefix(l.Price, "$"), 32)
+	return strconv.ParseFloat(l.Price, 64)
 }
 
 // parseAmiiboPresentedBy parses the presenter from the lineup.Amiibo.
@@ -246,9 +280,9 @@ func parseAmiiboSlug(l *lineup.Amiibo) string {
 	return l.Slug
 }
 
-// parseAmiiboTimestamp parses the unix timestamp from the lineup.Amiibo.
-func parseAmiiboTimestamp(l *lineup.Amiibo) time.Time {
-	return time.Unix(l.UnixTimestamp, 0).UTC()
+// parseAmiiboTimestamp parses the unix timestamp from either compatability.Amiibo or lineup.Amiibo.
+func parseAmiiboTimestamp(sec int64) time.Time {
+	return time.Unix(sec, 0).UTC()
 }
 
 // parseAmiiboTitle parses the title from the lineup.Item.
@@ -302,7 +336,7 @@ func parseLineup(a *Amiibo, l *lineup.Amiibo) {
 	a.Series = l.Series
 	a.SeriesID = parseAmiiboSeriesID(l)
 	a.Slug = parseAmiiboSlug(l)
-	a.Timestamp = parseAmiiboTimestamp(l)
+	a.Timestamp = parseAmiiboTimestamp(l.UnixTimestamp)
 	a.TypeAlias = parseAmiiboTypeAlias(l)
 	a.UPC = l.UPC
 	a.Unix = l.UnixTimestamp
@@ -311,7 +345,8 @@ func parseLineup(a *Amiibo, l *lineup.Amiibo) {
 // parseItem parses all fields exposed in the lineup.Item and assigns them to the argument amiibo.Amiibo.
 func parseItem(a *Amiibo, i *lineup.Item) {
 	a.Description = i.Description
-	a.LastModified = i.LastModified
+	a.LastModified = i.LastModified / 1000
+	a.LastModifiedTimestamp = parseAmiiboTimestamp(a.LastModified)
 	a.Path = i.Path
 	a.Title = i.Title
 	if a.URL == nil {
