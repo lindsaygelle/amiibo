73f7a83780b45c243be83c84eb6dfb697e36e115
gellel <lindsay.gelle@gmail.com>
2019-11-26 14:46:13 +1100

    Added documentation for amiibo/amiibo.Game

--- amiibo/game.go
+++ amiibo/game.go
@@ -5,6 +5,8 @@ import (
 	"github.com/gellel/amiibo/image"
 )
 
+// Game is a representation of a Nintendo video-game that is directly compatible
+// with a Nintendo Amiibo figurine.
 type Game struct {
 	Image *image.Image     `json:"image"`
 	Name  string           `json:"name"`
