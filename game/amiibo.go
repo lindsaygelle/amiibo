5e0d2d912103e0fc3fb13045bd5a247c08f4dbe7
gellel <lindsay.gelle@gmail.com>
2019-11-27 20:44:45 +1100

    Adding parsing for game content

--- game/amiibo.go
+++ game/amiibo.go
@@ -1,3 +1,157 @@
 package game
 
-type Amiibo struct{}
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+
+	"github.com/gellel/amiibo/address"
+	"github.com/gellel/amiibo/image"
+	"github.com/gellel/amiibo/resource"
+	t "github.com/gellel/amiibo/text"
+)
+
+type Amiibo struct {
+	Image           *image.Image     `json:"image"`
+	IsReleased      bool             `json:"is_released"`
+	Name            string           `json:"name"`
+	ReleaseDateMask string           `json:"release_date_mask"`
+	Series          string           `json:"series"`
+	Timestamp       time.Time        `json:"timestamp"`
+	URL             *address.Address `json:"url"`
+}
+
+func NewAmiibo(s *goquery.Selection) (*Amiibo, error) {
+	var (
+		ok bool
+	)
+	ok = (s != nil)
+	if !ok {
+		return nil, fmt.Errorf("*s is nil")
+	}
+	ok = (s.Length() != 0)
+	if !ok {
+		return nil, fmt.Errorf("*s is empty")
+	}
+	var (
+		amiibo = Amiibo{}
+	)
+	amiibo.Image, _ = parseAmiiboImage(s)
+	amiibo.Name, _ = parseAmiiboName(s)
+	amiibo.Series, _ = parseAmiiboSeries(s)
+	amiibo.URL, _ = parseAmiiboURL(s)
+	amiibo.ReleaseDateMask, _ = parseAmiiboReleaseDateMask(s)
+	amiibo.Timestamp, _ = time.Parse("01/02/2006", amiibo.ReleaseDateMask)
+	amiibo.Timestamp = amiibo.Timestamp.UTC()
+	amiibo.IsReleased = amiibo.Timestamp.Unix() < time.Now().UTC().Unix()
+	return &amiibo, nil
+}
+
+func parseAmiiboImage(s *goquery.Selection) (*image.Image, error) {
+	const (
+		CSS string = "img"
+	)
+	var (
+		ok     bool
+		rawurl string
+	)
+	s = (s.Find(CSS).First())
+	ok = (s.Length() != 0)
+	if !ok {
+		return nil, fmt.Errorf("*s is empty")
+	}
+	rawurl, ok = s.Attr("src")
+	if !ok {
+		return nil, fmt.Errorf("*s has no src")
+	}
+	return image.NewImage(fmt.Sprintf("%s%s", resource.Nintendo, rawurl))
+}
+
+func parseAmiiboName(s *goquery.Selection) (string, error) {
+	const (
+		CSS string = ".amiibo-name"
+	)
+	var (
+		err  error
+		name string
+		ok   bool
+	)
+	s = (s.Find(CSS).First())
+	ok = (s.Length() != 0)
+	if !ok {
+		return name, fmt.Errorf("*s is empty")
+	}
+	name = (s.Text())
+	ok = (len(name) != 0)
+	if !ok {
+		return name, fmt.Errorf("*s has no text")
+	}
+	return t.Name(name), err
+}
+
+func parseAmiiboReleaseDateMask(s *goquery.Selection) (string, error) {
+	const (
+		CSS string = "span[itemprop='releaseDate']"
+	)
+	var (
+		err       error
+		ok        bool
+		substring = (s.Find(CSS).Text())
+	)
+	substring = strings.TrimSpace(substring)
+	ok = (len(substring) != 0)
+	if !ok {
+		return substring, fmt.Errorf("*s is empty")
+	}
+	substring = strings.ToLower(substring)
+	substring = strings.ReplaceAll(substring, " ", "")
+	substring = strings.ReplaceAll(substring, "\n", "")
+	substring = strings.Replace(substring, "available", "", 1)
+	return substring, err
+}
+
+func parseAmiiboSeries(s *goquery.Selection) (string, error) {
+	const (
+		CSS string = "span[itemprop='isRelatedTo']"
+	)
+	var (
+		err    error
+		series string
+		ok     bool
+	)
+	s = (s.Find(CSS).First())
+	ok = (s.Length() != 0)
+	if !ok {
+		return series, fmt.Errorf("*s is empty")
+	}
+	series = (s.Text())
+	ok = (len(series) != 0)
+	if !ok {
+		return series, fmt.Errorf("*s has no text")
+	}
+	series = strings.TrimSpace(series)
+	return series, err
+}
+
+func parseAmiiboURL(s *goquery.Selection) (*address.Address, error) {
+	const (
+		CSS string = "a"
+	)
+	var (
+		ok     bool
+		rawurl string
+	)
+	s = s.Find(CSS)
+	ok = (s.Length() != 0)
+	if !ok {
+		return nil, fmt.Errorf("*s is empty")
+	}
+	rawurl, ok = s.Attr("href")
+	if !ok {
+		return nil, fmt.Errorf("*s has no href")
+	}
+	rawurl = fmt.Sprintf("%s%s", resource.Nintendo, rawurl)
+	return address.NewAddress(rawurl)
+}
--- game/game.go
+++ game/game.go
@@ -1,12 +1,24 @@
 package game
 
 import (
+	"fmt"
+	"net/http"
+	"strconv"
 	"time"
 
 	"golang.org/x/text/language"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/gellel/amiibo/address"
+	"github.com/gellel/amiibo/compatability"
 	"github.com/gellel/amiibo/image"
+	"github.com/gellel/amiibo/network"
+	"github.com/gellel/amiibo/resource"
+	t "github.com/gellel/amiibo/text"
+)
+
+const (
+	tep string = "%s%s" // tep string for raw url
 )
 
 const (
@@ -14,29 +26,138 @@ const (
 	Version string = "1.0.0"
 )
 
+var (
+	errNilAll = fmt.Errorf("*c, and *i are nil")
+)
+
 // Game is a structured representation of a Nintendo video-game that is compatible with a
-// Nintendo Amiibo figuring product. Game structs are built from a mixture of resources that
+// Nintendo Amiibo figuring product. Game structs are built from a mixture of resource that
 // are provided from the amiibo/mix package.
 // Games are consumed by amiibo/mux to create a basic HTTP REST API.
 type Game struct {
-	Compatability   []*Amiibo        `json:"compatability"`
-	Complete        bool             `json:"complete"`
-	Description     string           `json:"description"`
-	GamePath        string           `json:"game_path"`
-	GameURL         *address.Address `json:"game_url"`
-	ID              string           `json:"id"`
-	Image           *image.Image     `json:"image"`
-	IsReleased      bool             `json:"is_released"`
-	Language        language.Tag     `json:"language"`
-	LastModified    int64            `json:"last_modified"`
-	Path            string           `json:"path"`
-	Name            string           `json:"name"`
-	ReleaseDateMask string           `json:"release_date_mask"`
-	Timestamp       time.Time        `json:"timestamp"`
-	Title           string           `json:"title"`
-	Type            string           `json:"type"`
-	Unix            int64            `json:"unix"`
-	URI             string           `json:"uri"`
-	URL             *address.Address `json:"url"`
-	Version         string           `json:"version"`
+	Compatability         []*Amiibo        `json:"compatability"`
+	Complete              bool             `json:"complete"`
+	Description           string           `json:"description"`
+	GamePath              string           `json:"game_path"`
+	GameURL               *address.Address `json:"game_url"`
+	ID                    string           `json:"id"`
+	Image                 *image.Image     `json:"image"`
+	IsReleased            bool             `json:"is_released"`
+	Language              language.Tag     `json:"language"`
+	LastModified          int64            `json:"last_modified"`
+	LastModifiedTimestamp time.Time        `json:"last_modified_timestamp"`
+	Path                  string           `json:"path"`
+	Name                  string           `json:"name"`
+	ReleaseDateMask       string           `json:"release_date_mask"`
+	Timestamp             time.Time        `json:"timestamp"`
+	Title                 string           `json:"title"`
+	Type                  string           `json:"type"`
+	Unix                  int64            `json:"unix"`
+	URI                   string           `json:"uri"`
+	URL                   *address.Address `json:"url"`
+	Version               string           `json:"version"`
+}
+
+func NewGame(c *compatability.Game, i *compatability.Item) (*Game, error) {
+	var (
+		ok bool
+	)
+	ok = (c != nil) || (i != nil)
+	if !ok {
+		return nil, errNilAll
+	}
+	var (
+		g = &Game{
+			Language: language.AmericanEnglish}
+	)
+	if c != nil {
+		parseCompatability(g, c)
+	}
+	if i != nil {
+		parseItem(g, i)
+	}
+	g.Compatability, _ = parseGameCompatability(g.URL.URL)
+	g.Complete = c != nil && i != nil
+	g.URI = t.URI(g.Name)
+	return g, nil
+}
+
+func parseGameCompatability(rawurl string) ([]*Amiibo, error) {
+	const (
+		CSS string = "ul.figures li"
+	)
+	var (
+		req, _ = http.NewRequest(http.MethodGet, rawurl, nil)
+		res, _ = network.Client.Do(req)
+	)
+	var (
+		doc, err = goquery.NewDocumentFromResponse(res)
+	)
+	if err != nil {
+		return nil, err
+	}
+	var (
+		amiibo = []*Amiibo{}
+		s      = doc.Find(CSS)
+	)
+	s.Each(func(i int, s *goquery.Selection) {
+		var (
+			a, err = NewAmiibo(s)
+		)
+		if err != nil {
+			return
+		}
+		amiibo = append(amiibo, a)
+	})
+	return amiibo, err
+}
+
+// parseGameIsReleased parses the release state of the Nintendo game from the compatability.Game.
+func parseGameIsReleased(c *compatability.Game) (bool, error) {
+	return strconv.ParseBool(c.IsReleased)
+}
+
+// parseGameName parses the name of the game using the name field from either compatability.Game or compatability.Item.
+func parseGameName(s string) string {
+	return t.Name(s)
+}
+
+// parseGameReleaseDateMask parses the release date mask from the compatability.Game.
+func parseGameReleaseDateMask(c *compatability.Game) string {
+	return c.ReleaseDateMask
+}
+
+// parsGameTimestamp parses the unix timestamp from either compatability.Game or compatability.Item.
+func parsGameTimestamp(sec int64) time.Time {
+	return time.Unix(sec, 0).UTC()
+}
+
+// parseGameURL parses the URL of the game using the URL field from either compatability.Game or compatability.Item.
+func parseGameURL(rawurl string) (*address.Address, error) {
+	return address.NewAddress(rawurl)
+}
+
+// parseCompatability parses all fields exposed in the compatability.Game and assigns them to the argument game.Game.
+func parseCompatability(g *Game, c *compatability.Game) {
+	g.GamePath = c.Path
+	g.GameURL, _ = parseGameURL(fmt.Sprintf(tep, resource.Nintendo, c.URL))
+	g.ID = c.ID
+	g.Image, _ = image.NewImage(fmt.Sprintf(tep, resource.Nintendo, c.Image))
+	g.IsReleased, _ = parseGameIsReleased(c)
+	g.Name = parseGameName(c.Name)
+	g.Path = c.Path
+	g.ReleaseDateMask = parseGameReleaseDateMask(c)
+	g.Timestamp, _ = time.Parse("2006-01-02", c.ReleaseDateMask)
+	g.Type = c.Type
+	g.Unix = g.Timestamp.Unix()
+}
+
+// parseItem parses all fields exposed in the compatability.Item and assigns them to the argument game.Game.
+func parseItem(g *Game, i *compatability.Item) {
+	g.Description = i.Description
+	g.LastModified = i.LastModified / 1000
+	g.LastModifiedTimestamp = parsGameTimestamp(g.LastModified)
+	g.Path = i.Path
+	g.Title = i.Title
+	g.URL, _ = parseGameURL(i.URL)
 }
