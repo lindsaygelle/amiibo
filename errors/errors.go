7f810353a3bd240f7dd78566a7fb30104b69cb93
gellel <lindsay.gelle@gmail.com>
2019-11-28 22:44:22 +1100

    Adding documentation to errors/

--- errors/errors.go
+++ errors/errors.go
@@ -26,39 +26,51 @@ const (
 )
 
 var (
+	//ErrGoQueryEmpty is an error for an empty goquery.Selection.
 	ErrGoQueryEmpty = fmt.Errorf(errGoQueryEmpty)
 )
 var (
+	// ErrGoQueryNoHref is an error for a goquery.Selection without a href attribute.
 	ErrGoQueryNoHref = fmt.Errorf(errGoQueryNoHref)
 )
 var (
+	// ErrGoQueryNoSrc is an error for a goquery.Selection without a src attribute.
 	ErrGoQueryNoSrc = fmt.Errorf(errGoQueryNoSrc)
 )
 var (
+	// ErrGoQueryNoText is an error for a goquery.Selection without text nodes.
 	ErrGoQueryNoText = fmt.Errorf(errGoQueryNoText)
 )
 var (
+	// ErrGoQueryNoTitle is an error for a goquery.Selection without a title attribute.
 	ErrGoQueryNoTitle = fmt.Errorf(errGoQueryNoTitle)
 )
 
 var (
+	//ErrArgCNil is an error when pointer to argument c is nil.
 	ErrArgCNil = fmt.Errorf(errArgCNil)
 )
 var (
+	//ErrArgGNil is an error when pointer to argument g is nil.
 	ErrArgGNil = fmt.Errorf(errArgGNil)
 )
 var (
+	//ErrArgINil is an error when pointer to argument i is nil.
 	ErrArgINil = fmt.Errorf(errArgINil)
 )
 var (
+	//ErrArgLNil is an error when pointer to argument l is nil.
 	ErrArgLNil = fmt.Errorf(errArgLNil)
 )
 var (
+	//ErrArgSNil is an error when pointer to argument s is nil.
 	ErrArgSNil = fmt.Errorf(errArgSNil)
 )
 var (
+	//ErrArgsNil is an error when all arguments that are pointers are nil.
 	ErrArgsNil = fmt.Errorf(errArgsNil)
 )
 var (
+	// ErrArgsNoRel is an error when all arguments (usually to mix) have no common struct value data point.
 	ErrArgsNoRel = fmt.Errorf(errArgsNoRel)
 )
