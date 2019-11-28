a5abdb77b8ef87999d65c45aaca37b528b82ecf9
gellel <lindsay.gelle@gmail.com>
2019-11-28 20:34:03 +1100

    Added HTTP network timeout

--- network/client.go
+++ network/client.go
@@ -1,8 +1,17 @@
 package network
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+)
+
+const (
+	// Timeout is the default HTTP client timeout in seconds.
+	Timeout time.Duration = time.Second * 5
+)
 
 var (
 	// Client is the default HTTP client used for contacting the *://*.nintendo.com/*.
-	Client = (&http.Client{})
+	Client = (&http.Client{
+		Timeout: Timeout})
 )
