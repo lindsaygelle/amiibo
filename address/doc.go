b7a63ad4cc6632b3db96936befa95adc6e4938a4
gellel <lindsay.gelle@gmail.com>
2019-12-05 20:14:34 +1100

    Adding documenation

--- address/doc.go
+++ address/doc.go
@@ -1,2 +1,5 @@
-// Package address is a URL parses for the amiibo package.
+// Package address is a URL parser for the amiibo package.
+//
+// Address handles the deconstruction of URL's found while
+// parsing the Nintendo Amiibo XHR content.
 package address
