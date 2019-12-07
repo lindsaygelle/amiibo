1f112336152568d3da6a2bdc57df9951527f6222
gellel <lindsay.gelle@gmail.com>
2019-12-07 21:45:55 +1100

    Adding basic doc.go file

---
+++ network/doc.go
@@ -0,0 +1,2 @@
+// Package network is the basic HTTP layer used by the amiibo SDK.
+package network
---
+++ resource/doc.go
@@ -0,0 +1,2 @@
+// Package resource is the aggregation of all external URLs contacted by the amiibo SDK.
+package resource
