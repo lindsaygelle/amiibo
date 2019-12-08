c90578a33c8d2ccfffead3f112a7588421d16c9b
Lindsay Gelle <lindsay.gelle@gmail.com>
2019-12-08 20:43:53 +1100

    Update doc.go

--- amiibo/doc.go
+++ amiibo/doc.go
@@ -3,13 +3,14 @@
 // To create a standardized Amiibo (a standardized Amiibo is an Amiibo that is composed of all
 // of the common amiibo SDK components), the appropriate compatability and lineup
 // information need to be found across all of the exposed fields in the various
-// amiibo SDK XHR structs.
+// amiibo SDK XHR structs and merged into the one Amiibo struct.
 //
-// This package does not assume the responsibility of performing this action (the data union mentioned above),
+// This package does not assume the responsibility of performing this action (the data composition mentioned above),
 // and allows the developer to build their own custom union, passing in
-// the relevant structs that help detail the level of verbosity each standardized Amiibo should export.
+// the relevant structs that help control the level of verbosity each 
+// standardized Amiibo should export.
 //
-// The amiibo SDK provides a basic union operation that will automatically compose
-// and parse the structs into a normalized game. Refer to the package documenation for
+// The amiibo SDK provides a basic composition operation that will automatically search
+// and parse the structs into a normalized Amiibo. Refer to the package documentation for
 // more information.
 package amiibo
