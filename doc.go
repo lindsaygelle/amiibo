0aa0f807f74abf689a96fed816396495a3774501
Lindsay Gelle <lindsay.gelle@gmail.com>
2019-12-08 20:52:17 +1100

    Update doc.go

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-// Package amiibo is an unofficial Nintendo SDK for the Go programming language.
+// Package amiibo is an unofficial SDK for the Go programming language.
 //
 // The amiibo SDK provides tools that developers can use to explore the Amiibo products
 // on offer from Nintendo.
@@ -9,4 +9,15 @@
 // The SDK also includes helpful utilities on top of the core struct layers that add additional
 // capabilities and functionality. For example, the amiibo/mux package provides a
 // basic HTTP mux interface that can be extended and hosted on an public service.
+//
+// To help break the package into smaller consumable and maintainable parts, most of the functionality
+// has been separated into various subpackages. Each subpackage is intended to handle a particular
+// level of complexity. To consume the amiibo directly from the Nintendo CDN, the amiibo/compatability
+// and amiibo/lineup packages are the best places to start, as these items focus on contacting the
+// Nintendo Amiibo website directly (through the use of the resources contained in the resources
+// package).
+//
+// To use the normalized Nintendo data, the amiibo/web package can be imported and used. The web
+// package focuses on abstracting the task of fetching, merging and organizing all of
+// the Nintendo data across all of the different resources.
 package amiibo
