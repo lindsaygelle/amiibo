f6bc44cd4c185f1dfff606c35a662f5b9b9f29c0
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-04 15:08:13 +1000

    Added to amiibo.go

--- amiibo.go
+++ amiibo.go
@@ -1,1 +1,16 @@
+// Package amiibo is an unofficial SDK for the Go programming language.
 package amiibo
+
+// amiibo is the normalized amiibo data scraped from a rawAmiibo.
+type amiibo struct{}
+
+// amiiboCompatability is the unfettered Nintendo Amiibo compatability information provided by nintendo.com.
+type amiiboCompatability struct {
+	AmiiboList    []rawAmiibo `json:"amiiboList"`
+	ComponentPath string      `json:"componentPath"`
+	Language      string      `json:"language"`
+}
+
+// rawAmiibo is the unfettered amiibo product data provided by nintendo.com.
+type rawAmiibo struct{}
+
