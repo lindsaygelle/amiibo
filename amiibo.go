064e59b219d6f049cf8146584edd59c3fc6b9d27
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-05 00:35:42 +1000

    Added to amiibo.go

--- amiibo.go
+++ amiibo.go
@@ -8,13 +8,33 @@ type amiibo struct{}
 // amiiboCompatibility contains the relationship information between Nintendo Amiibo products
 // and the games or applications that can be used within.
 type amiiboCompatibility struct {
-	AuthorMode    bool                  `json:"authorMode"`
-	AmiiboList    []compatabilityAmiibo `json:"amiiboList"`
-	ComponentPath string                `json:"componentPath"`
-	GameList      []compatabilityGame   `json:"gameList"`
-	Items         []compatabilityItem   `json:"items"`
-	Language      string                `json:"language"`
-	Mode          string                `json:"mode"`
+
+	// AuthorMode is the state of the Nintendo CDN for the compatability file.
+	AuthorMode bool `json:"authorMode"`
+
+	// AmiiboList is a collection of Nintendo Amiibo products that are compatible with Nintendo games.
+	AmiiboList []compatabilityAmiibo `json:"amiiboList"`
+
+	// ComponentPath is the relative path to the Nintendo compatability file.
+	ComponentPath string `json:"componentPath"`
+
+	// Date is the date signature when the Nintendo compatability file was recevied.
+	Date string `json:"date"`
+
+	// ETag is the cache ID for the Nintendo compatability file.
+	ETag string `json:"etag"`
+
+	// GameList is a collection of Nintendo games that are compatible with Nintendo Amiibo products.
+	GameList []compatabilityGame `json:"gameList"`
+
+	// Items is a collection of related metadata that corresponds to Nintendo games that support Nintendo Amiibo.
+	Items []compatabilityItem `json:"items"`
+
+	// Language is the ISO key for the language the Nintendo information is presented in.
+	Language string `json:"language"`
+
+	// Mode is an exposed field from the Nintendo CDN that has an unknown purpose.
+	Mode string `json:"mode"`
 }
 
 // compatabilityAmiibo is the unfettered Nintendo Amiibo product data provided by nintendo.com.
@@ -26,34 +46,16 @@ type compatabilityAmiibo struct {
 
 	// compatabilityCommon is a composed property.
 	compatabilityCommon
-
-	// ID is the unique identifier for the Nintendo Amiibo figurine or card.
-	//
-	// ID is a UUID.
-	ID string `json:"id"`
+	// compatabilityCommonProduct is a composed property.
+	compatabilityCommonProduct
 
 	// IsRelatedTo is the name of a Nintendo product or series that the Nintendo Amiibo figurine or card is affiliated with.
 	//
 	// IsRelatedTo often can contain special characters that will need filtering to prevent poor hashing keys.
 	IsRelatedTo string `json:"isRelatedTo"`
 
-	// Image is the relative URL to the Nintendo Amiibo box art image.
-	//
-	// Image requires the nintendo.com domain prepended to the URL.
-	Image string `json:"image"`
-
-	// Name is the name given to the Nintendo game.
-	//
-	// Name often can contain special characters that will need filtering to prevent poor hashing keys.
-	Name string `json:"name"`
-
 	// TagID is the identifier namespace for the Nintendo Amiibo figurine or card.
 	TagID string `json:"tagId"`
-
-	// Type is the entity type for the Nintendo Amiibo product.
-	//
-	// Type can be type amiibo or other for Nintendo Amiibo products.
-	Type string `json:"type"`
 }
 
 // compatabilityCommon is the common properties shared between compatabilityAmiibo, compatabilityGame and compatabilityItem structs.
@@ -73,17 +75,10 @@ type compatabilityCommon struct {
 	URL string `json:"url"`
 }
 
-// compatabilityGame is the unfettered game information related to a Nintendo Amiibo product provided by nintendo.com.
-//
-// compatabilityGame describes the abbreviated game product information that has known Nintendo Amiibo support.
-//
-// compatabilityGame contains varying levels of accuracy relative to the release status of Nintendo Amiibo products.
-type compatabilityGame struct {
+// compatabilityCommonProduct are the common product properties shared between compatabilityAmiibo and compatabilityGame.
+type compatabilityCommonProduct struct {
 
-	// compatabilityCommon is a composed property.
-	compatabilityCommon
-
-	// ID is the unique identifier for the Nintendo game.
+	// ID is the unique identifier for the Nintendo product.
 	//
 	// ID is a UUID.
 	ID string `json:"id"`
@@ -93,20 +88,31 @@ type compatabilityGame struct {
 	// Image requires the nintendo.com domain prepended to the URL.
 	Image string `json:"image"`
 
-	// IsReleased is a string representation of a boolean that indicates whether the Nintendo game is available.
+	// IsReleased is a string representation of a boolean that indicates whether the Nintendo product is available.
 	//
 	// IsReleased needs to be formatted to a bool data type.
 	IsReleased string `json:"isReleased"`
 
-	// Name is the name given to the Nintendo game.
+	// Name is the name given to the Nintendo product.
 	//
 	// Name often can contain special characters that will need filtering to prevent poor hashing keys.
 	Name string `json:"name"`
 
 	// Type is the entity type for the Nintendo product.
-	//
-	// Type is always type game for Nintendo game products.
 	Type string `json:"type"`
+}
+
+// compatabilityGame is the unfettered game information related to a Nintendo Amiibo product provided by nintendo.com.
+//
+// compatabilityGame describes the abbreviated game product information that has known Nintendo Amiibo support.
+//
+// compatabilityGame contains varying levels of accuracy relative to the release status of Nintendo Amiibo products.
+type compatabilityGame struct {
+
+	// compatabilityCommon is a composed property.
+	compatabilityCommon
+	// compatabilityCommonProduct is a composed property.
+	compatabilityCommonProduct
 }
 
 // compatabilityItem is the unfettered auxiliary information related to a Nintendo Amiibo product provided by nintendo.com.
