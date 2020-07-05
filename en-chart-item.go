c91523dc507d51240aec72b6a29917b6e84620f2
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-05 23:46:17 +1000

    Added to en-chart-item.go

--- en-chart-item.go
+++ en-chart-item.go
@@ -7,11 +7,21 @@ import (
 
 // ENGChartItem is the unfettered English language Nintendo Amiibo additional product information.
 type ENGChartItem struct {
-	Description  string `json:"description"`
-	LastModified int64  `json:"lastModified"`
-	Path         string `json:"path"`
-	Title        string `json:"title"`
-	URL          string `json:"url"`
+
+	// Description is the Nintendo Amiibo item product description.
+	Description string `json:"description"`
+
+	// LastModified is the Nintendo Amiibo item product release date in milliseconds.
+	LastModified int64 `json:"lastModified"`
+
+	// Path is the relative path to the Nintendo Amiibo item product page.
+	Path string `json:"path"`
+
+	// Title is the unformatted name of the Nintendo Amiibo item product.
+	Title string `json:"title"`
+
+	// URL is the relative path to the Nintendo Amiibo item page.
+	URL string `json:"url"`
 }
 
 // GetID returns a ID for the ENGChartItem.
