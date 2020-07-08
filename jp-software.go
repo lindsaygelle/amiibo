14aa41ab4b04c12bc594781e1553734f5bf5f004
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-08 20:34:38 +1000

    Added to jp-software.go

--- jp-software.go
+++ jp-software.go
@@ -9,6 +9,9 @@ type JPNSoftware struct {
 	ID              string
 	Name            string
 	NameAlternative string
+	Platform        string
+	Price           string
+	Priority        string
 	ReleaseDate     time.Time
 }
 
@@ -16,6 +19,9 @@ func (j *JPNSoftware) Add(v *JPNChartSoftwareItem) (err error) {
 	j.ID = v.Code
 	j.Name = v.Name
 	j.NameAlternative = v.NameKana
+	j.Platform = v.Series
+	j.Price = v.Price
+	j.Priority = v.Priority
 	var l = len(v.Date)
 	var date = fmt.Sprintf("%s-%s-%s", v.Date[:4], v.Date[l-4:l-2], v.Date[l-2:])
 	var releaseDate time.Time
