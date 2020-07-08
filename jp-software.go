94ef6dcb3c10f3ec7362d4fc32dc758e47b3f8ae
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-08 20:31:58 +1000

    Added jp-software.go

---
+++ jp-software.go
@@ -0,0 +1,27 @@
+package amiibo
+
+import (
+	"fmt"
+	"time"
+)
+
+type JPNSoftware struct {
+	ID              string
+	Name            string
+	NameAlternative string
+	ReleaseDate     time.Time
+}
+
+func (j *JPNSoftware) Add(v *JPNChartSoftwareItem) (err error) {
+	j.ID = v.Code
+	j.Name = v.Name
+	j.NameAlternative = v.NameKana
+	var l = len(v.Date)
+	var date = fmt.Sprintf("%s-%s-%s", v.Date[:4], v.Date[l-4:l-2], v.Date[l-2:])
+	var releaseDate time.Time
+	releaseDate, err = time.Parse("2006-01-02", date)
+	if err == nil {
+		j.ReleaseDate = releaseDate
+	}
+	return
+}
