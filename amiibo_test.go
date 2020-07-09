e45241b9588aeb0d80012cccaba3cb71d6d697e7
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-09 16:38:24 +1000

    Added to amiibo_test.go

--- amiibo_test.go
+++ amiibo_test.go
@@ -3,7 +3,24 @@ package amiibo_test
 import (
 	"path/filepath"
 	"runtime"
+	"testing"
+
+	"github.com/lindsaygelle/amiibo"
 )
 
 var _, caller, _, _ = runtime.Caller(0)
 var filefolder = filepath.Dir(caller)
+
+func TestAmiibo(t *testing.T) {
+	var name = "name"
+	var nameAlt = "nameAlt"
+	var engAmiibo amiibo.Amiibo = amiibo.ENGAmiibo{
+		Name:            name,
+		NameAlternative: nameAlt}
+	if getName := engAmiibo.GetName(); getName != name {
+		t.Fatalf("(Amiibo).GetName() %s != %s", getName, name)
+	}
+	if getNameAlternative := engAmiibo.GetNameAlternative(); getNameAlternative != nameAlt {
+		t.Fatalf("(Amiibo).GetNameAlternative() %d != %s", getNameAlternative, nameAlt)
+	}
+}
