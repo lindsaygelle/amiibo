d49babc4a8f30acdda49da443dc55c6f9bc673ea
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-11 11:48:16 +1000

    Added to jp-software-map.go

--- jp-software-map.go
+++ jp-software-map.go
@@ -5,6 +5,39 @@ import "fmt"
 // JPNSoftwareMap is a map of JPNSoftware.
 type JPNSoftwareMap (map[string]JPNSoftware)
 
+// Add adds a JPNSoftware to the JPNSoftwareMap.
+func (j *JPNSoftwareMap) Add(v *JPNSoftware) (ok bool) {
+	(*j)[v.GetID()] = *v
+	ok = j.Has(v.GetID())
+	return
+}
+
+// Del deletes a JPNSoftware from the JPNSoftwareMap.
+func (j *JPNSoftwareMap) Del(ID string) (ok bool) {
+	delete(*j, ID)
+	ok = j.Has(ID) == false
+	return
+}
+
+// Each performs a for-each loop through the JPNSoftwareMap.
+func (j *JPNSoftwareMap) Each(f func(string, JPNSoftware)) {
+	for k, v := range *j {
+		f(k, v)
+	}
+}
+
+// Get gets an JPNSoftware from the JPNSoftwareMap.
+func (j *JPNSoftwareMap) Get(ID string) (v JPNSoftware, ok bool) {
+	v, ok = (*j)[ID]
+	return
+}
+
+// Has checks if the JPNSoftwareMap has a JPNSoftware with the corresponding ID.
+func (j *JPNSoftwareMap) Has(ID string) (ok bool) {
+	_, ok = j.Get(ID)
+	return
+}
+
 // NewJPNSoftwareMap returns a new JPNSoftwareMap.
 func NewJPNSoftwareMap(JPNChartSoftware *JPNChartSoftware) (v JPNSoftwareMap, err error) {
 	v = (make(JPNSoftwareMap))
@@ -15,7 +48,7 @@ func NewJPNSoftwareMap(JPNChartSoftware *JPNChartSoftware) (v JPNSoftwareMap, er
 		}
 		var p, ok = v[ID]
 		if !ok {
-			err = fmt.Errorf("JPNSoftwareMap[JPNChartSoftware.Items.GetID()] != ok")
+			err = fmt.Errorf("(JPNChartSoftware)[ID]: false")
 		}
 		if err != nil {
 			return
