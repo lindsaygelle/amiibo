2cc1b68611cba8a6801d1a88db737fd9e0acdabe
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-11 11:56:44 +1000

    Added to amiibo.go

--- amiibo.go
+++ amiibo.go
@@ -10,8 +10,17 @@ import (
 //
 // Amiibo provides a handler to get all common fields between the ENGAmiibo and JPNAmiibo.
 type Amiibo interface {
+
+	// GetAvailable returns the Amiibo's availability status.
+	//
+	// GetAvailable is calculated by evaluating the Amiibo's release date is
+	// before the current timestamp.
 	GetAvailable() bool
+
+	// GetID returns the Amiibo's hash key ID.
 	GetID() string
+
+	// GetLanguage returns the Amiibo's language identifier.
 	GetLanguage() language.Tag
 	GetMD5() (string, []byte, error)
 	GetName() string
