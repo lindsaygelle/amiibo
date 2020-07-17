e5061c5990801a31c4c9dd27420a4a831033b304
lindsaygelle <lindsay.gelle@gmail.com>
2020-07-17 13:55:15 +1000

    Added to image.go

--- image.go
+++ image.go
@@ -1,7 +1,14 @@
 package amiibo
 
 import (
+	"fmt"
 	"image"
+	"image/gif"
+	"image/jpeg"
+	"image/png"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 // Image is an image resource from Nintendo.
@@ -25,5 +32,27 @@ func NewImage(URL string) (v Image, err error) {
 	v.Image = i
 	v.Height = r.Y
 	v.Width = r.Y
+	return
+}
+
+// WriteImage writes an Image to disc.
+func WriteImage(dir string, filename string, v *Image) (fullpath string, err error) {
+	var f *os.File
+	fullpath = filepath.Join(dir, fmt.Sprintf("%s.%s", filename, strings.ToLower(v.Ext)))
+	f, err = os.Create(fullpath)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	switch v.Ext {
+	case "GIF":
+		err = gif.Encode(f, v.Image, nil)
+	case "JPG":
+		err = jpeg.Encode(f, v.Image, nil)
+	case "PNG":
+		err = png.Encode(f, v.Image)
+	default:
+		err = fmt.Errorf("unsupported image format")
+	}
 	return
 }
